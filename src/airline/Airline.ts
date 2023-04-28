928a22b6bc3b70e0f46dbc73364d31142ca92f5b
hayya <remhay07@gmial.com>
2023-04-28 14:51:05 +0700

    [Add Aeroplan into Airline]

--- src/airline/Airline.ts
+++ src/airline/Airline.ts
@@ -1,5 +1,9 @@
 import { Aeroplan } from "./aeroplan/Aeroplan"
 export class Airline{
     private name: string
-    private aeroplane: Aeroplan[]=[]
+    private aeroplan: Aeroplan[]=[]
+
+    addAeroplan(aeroplan: Aeroplan) {
+        return this.aeroplan.push(aeroplan)
+    }
 }
