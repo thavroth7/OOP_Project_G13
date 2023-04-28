bfcbe04eb100b3bf73403bc3a2556d5eee3cdf7d
hayya <remhay07@gmial.com>
2023-04-28 14:56:03 +0700

    [Add seats into aeroplan]

--- src/airline/aeroplan/Aeroplan.ts
+++ src/airline/aeroplan/Aeroplan.ts
@@ -1,7 +1,14 @@
+import { Seat } from "./Seat"
+
 export class Aeroplan{
     private registrationNumber: string
+    private seats: Seat[]=[]
     constructor(registrationNumber: string)
     {
         this.registrationNumber = registrationNumber
+    }
+
+    addSeats(seat:Seat) {
+        return this.seats.push(seat)
     }
 }
