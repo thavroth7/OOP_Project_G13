51405d6a9b50db83112dd1b5ad420b63c2107ecc
THAVROTH.CHEA <THAVROTH.CHEA@PNC.LAN>
2023-04-29 17:04:03 +0700

    [Update class BookingTrip]

--- src/booking-system/trip/BookingTrip.ts
+++ src/booking-system/trip/BookingTrip.ts
@@ -1,23 +1,46 @@
 import { Departure } from "../../departure/Departure"
-
+import { Passenger } from "../../human/passenger/Passenger"
+import { Flight } from "../flight/Flight"
+import { Bag } from "./Bag"
 
 export enum TypeOfTicket{
         RETURN_TICKET,
         SINGLE_TICKET,
 }
 export class BookingTrip{
-    
-  
+    private bookingReferenceNumber: string
+    private price:number
+    private bag?: Bag
+    private flights: Flight[]=[]
+    private passengers: Passenger
     private departurePlace:Departure
     private arrivePlace: Departure
     private typeOfTicket: TypeOfTicket
-    constructor(departurePlace:Departure, arrivalPlace: Departure, typeOfTicket: TypeOfTicket)
+    constructor(bookingReferenceNumber: string, price:number, passenger:Passenger, departurePlace:Departure, arrivalPlace: Departure, typeOfTicket: TypeOfTicket)
     {
+        this.bookingReferenceNumber= bookingReferenceNumber
+        this.price = price
+        this.passengers = passenger
         this.departurePlace = departurePlace
         this.arrivePlace = arrivalPlace
         this.typeOfTicket = typeOfTicket
+        
     }
-   
+    addBookingFlight(flight: Flight){
+        this.flights.push(flight)
+    }
 
+    
+    setBag(bag:Bag){
+        this.bag = bag
 
+    }
+
+    getPassenger(){
+        return this.passengers
+    }
+
+    getReturnTicket(){
+        return TypeOfTicket.RETURN_TICKET
+    }
 }
