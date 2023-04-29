aa807d89df77f9cfad52ced78a7e9af6c544c918
THAVROTH.CHEA <THAVROTH.CHEA@pnc.lan>
2023-04-29 23:34:57 +0700

    [Update in Main class]

--- src/Main.ts
+++ src/Main.ts
@@ -19,19 +19,19 @@ import { Employee, EmployeeCategory } from "./human/employee/Employee";
 import { Pilot } from "./human/employee/Pilot";
 import { Passenger } from "./human/passenger/Passenger";
 
-
-
-
-
-
-
+//create departure//
 let departurePlace = new Departure("Phnom Penh", "Cambodia");
 let arrivePlace = new Departure("Seoul", "South Korea");
 
+
+//create gate//
 let gate1 = new Gate("2A")
 let gate2 = new Gate("001")
 
+//create route//
 let route = new Route(departurePlace, arrivePlace)
+
+//create aeroplan//
 let aeroplane = new Aeroplan("HMA")
 
 //create Date Time
@@ -69,21 +69,25 @@ pilot1.addFlight(flight2)
 pilot1.addFlight(flight3)
 pilot1.addFlight(flight4)
 
-// console.log(pilot1.findPilotFlights(date1))
+//Find Pilot's flight for given day//
+console.log(pilot1.findPilotFlights(date1))
 
-
+//create employee//------
 let employee1 = new Employee(EmployeeCategory.PILOT,"Hay",20,Gender.FEMALE)
 employee1.setSalary(1000)
 
 let employee2 = new Employee(EmployeeCategory.CO_PILOT,"Thavroth",20,Gender.FEMALE)
 employee2.setSalary(2000)
 
+//create airline//
 let airline1= new Airline("Angkor Airline")
 airline1.addEmployee(employee1)
 airline1.addEmployee(employee2)
-// console.log(airline1.findSalaryForAllEmployees())
 
-//add passenger//
+//find all employee salary that airline need to pay//
+console.log(airline1.findSalaryForAllEmployees())
+
+//Create passenger//
 let passenger1 = new Passenger("Jimin", 27, Gender.MALE)
 let passenger2 = new Passenger("Suga", 30, Gender.MALE)
 let passenger3 = new Passenger("Suga", 30, Gender.MALE)
@@ -93,13 +97,13 @@ passenger2.setMeal(meal1)
 
 
 //crate booking//
-let bag = new Bag(10)
 let bookingTrip = new BookingTrip( "AAS",1000,passenger1 ,departurePlace, arrivePlace, TypeOfTicket.RETURN_TICKET)
 let bookingTrip1 = new BookingTrip( "AAS",1000,passenger1 ,departurePlace, arrivePlace, TypeOfTicket.RETURN_TICKET)
 let bookingTrip2 = new BookingTrip( "AAS",1000,passenger1 ,departurePlace, arrivePlace, TypeOfTicket.RETURN_TICKET)
 let bookingTrip3 = new BookingTrip( "AAS",1000,passenger2 ,departurePlace, arrivePlace, TypeOfTicket.SINGLE_TICKET)
 let bookingTrip4 = new BookingTrip( "AAS",1000,passenger2 ,departurePlace, arrivePlace, TypeOfTicket.SINGLE_TICKET)
 //set bag
+let bag = new Bag(10)
 bookingTrip.setBag(bag)
 
 // add flight into trip
@@ -115,7 +119,7 @@ let airport = new Airport("Phnom Penh International Airport")
 airport.addBookingTrip(bookingTrip)
 
 //find passenger's trip//
-//  console.log(airport.findPassengerTrip(passenger1))
+ console.log(airport.findPassengerTrip(passenger1))
 
 
 
@@ -131,10 +135,10 @@ airline1.addFlight(flight1)
 airline1.addFlight(flight2)
 
 //Find gate that passenger will go//
-// console.log(passenger1.findGate())
+console.log(passenger1.findGate())
 
 //find number of passenger that booked return ticket//
-// console.log(airline1.findPassengerBookedReturnTicket())
+console.log(airline1.findPassengerBookedReturnTicket())
 
 //find type of meal Chef need to prepare for a given flight//--------------
 console.log(airline1.findTypeOfMeal(flight1))
